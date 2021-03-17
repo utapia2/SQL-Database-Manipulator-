4049e8b487ed5015d8317f9ac38b27edab92c68c
utapia2 <71459839+utapia2@users.noreply.github.com>
2021-03-16 19:58:13 -0500

    Add files via upload

---
+++ Tester/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace Tester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.DisplayButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputLast = new System.Windows.Forms.TextBox();
+            this.InsertButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InputFirst = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.InputAge = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(46, 66);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(109, 37);
+            this.ConnectButton.TabIndex = 0;
+            this.ConnectButton.Text = "Connect to DB";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // DisplayButton
+            // 
+            this.DisplayButton.Location = new System.Drawing.Point(46, 134);
+            this.DisplayButton.Name = "DisplayButton";
+            this.DisplayButton.Size = new System.Drawing.Size(109, 34);
+            this.DisplayButton.TabIndex = 5;
+            this.DisplayButton.Text = "Display All Data";
+            this.DisplayButton.UseVisualStyleBackColor = true;
+            this.DisplayButton.Click += new System.EventHandler(this.DisplayButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(373, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Last Name:";
+            // 
+            // inputLast
+            // 
+            this.inputLast.Location = new System.Drawing.Point(440, 63);
+            this.inputLast.Name = "inputLast";
+            this.inputLast.Size = new System.Drawing.Size(125, 20);
+            this.inputLast.TabIndex = 1;
+            // 
+            // InsertButton
+            // 
+            this.InsertButton.Location = new System.Drawing.Point(490, 180);
+            this.InsertButton.Name = "InsertButton";
+            this.InsertButton.Size = new System.Drawing.Size(75, 23);
+            this.InsertButton.TabIndex = 4;
+            this.InsertButton.Text = "Enter";
+            this.InsertButton.UseVisualStyleBackColor = true;
+            this.InsertButton.Click += new System.EventHandler(this.InsertButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(374, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "First Name:";
+            // 
+            // InputFirst
+            // 
+            this.InputFirst.Location = new System.Drawing.Point(440, 102);
+            this.InputFirst.Name = "InputFirst";
+            this.InputFirst.Size = new System.Drawing.Size(125, 20);
+            this.InputFirst.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(405, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Age:";
+            // 
+            // InputAge
+            // 
+            this.InputAge.Location = new System.Drawing.Point(440, 134);
+            this.InputAge.Name = "InputAge";
+            this.InputAge.Size = new System.Drawing.Size(125, 20);
+            this.InputAge.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 457);
+            this.Controls.Add(this.InputAge);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.InputFirst);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.InsertButton);
+            this.Controls.Add(this.inputLast);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DisplayButton);
+            this.Controls.Add(this.ConnectButton);
+            this.Name = "Form1";
+            this.Text = "dunder mifflin database";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Button DisplayButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputLast;
+        private System.Windows.Forms.Button InsertButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox InputFirst;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox InputAge;
+    }
+}
+
---
+++ Tester/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Tester
+{
+    public partial class Form1 : Form
+    {
+        static string mycnnstring = "Data Source = ULISSESPC; " +
+                "Initial Catalog = test; Integrated Security = True; " +
+                "Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; " +
+                "ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+        SqlConnection cnn = new SqlConnection(mycnnstring);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            
+            cnn.Open();
+            MessageBox.Show("You Connected to your database Customers!");
+
+        }
+
+        private void DisplayButton_Click(object sender, EventArgs e)
+        {
+            
+            string output = "";
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Customers", cnn);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                output = output + reader.GetValue(0) + "-" + reader.GetValue(1) + " " + reader.GetValue(2) + " " + reader.GetValue(3) + "\n";
+            }
+
+            DialogResult dialogResult = MessageBox.Show(output);
+            
+            reader.Close();
+            cmd.Dispose();
+        }
+
+        private void InsertButton_Click(object sender, EventArgs e)
+        {
+            
+            string lastID = "";
+            string lastname = inputLast.Text;
+            string firstname = InputFirst.Text;
+            string age = InputAge.Text;
+
+            SqlCommand cmd = new SqlCommand("SELECT TOP 1 * FROM Customers ORDER BY ID DESC", cnn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            while (reader.Read())
+            {
+                lastID = reader.GetValue(0).ToString();
+            }
+            reader.Close();
+
+            int nextId = Int32.Parse(lastID) + 1;
+            lastID = nextId.ToString();
+
+            string insertSQL = "INSERT INTO Customers VALUES ('" + lastID + "', '" + lastname + "', '" + firstname + "', '" + age + "');";
+
+            cmd = new SqlCommand(insertSQL, cnn);
+
+            adapter.InsertCommand = new SqlCommand(insertSQL, cnn);
+            adapter.InsertCommand.ExecuteNonQuery();
+
+            reader.Close();
+            cmd.Dispose();
+            
+
+        }
+    }
+}
